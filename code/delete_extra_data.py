39d0d9042f6d8d9b64de95f1ac9e64727609e740
halchil <halzcreate1215@gmail.com>
2024-07-24 22:35:05 +0900

    up

---
+++ code/delete_extra_data.py
@@ -0,0 +1,9 @@
+# パッケージの読み込み
+import numpy as np
+import pandas as pd
+
+# csvデータの取り込み(これは抽象的なオブジェクトのため、表示などはできない)
+train = pd.read_csv("/home/mainte/kaggle_titanic/data/train.csv")
+test = pd.read_csv("/home/mainte/kaggle_titanic/data/test.csv")
+
+print(train.head())
